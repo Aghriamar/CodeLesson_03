c22eaa18b8e776d50b51399190e57824be8fee41
Aghriamar <A1exXx88@inbox.ru>
2024-05-05 11:32:17 +0300

    Added homework_03

--- CodeLesson_03/CodeLesson_03.cpp
+++ CodeLesson_03/CodeLesson_03.cpp
@@ -1,6 +1,11 @@
 ﻿#include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    std::string myString = "Hello, world!";
+    std::cout << "String: " << myString << std::endl;
+    std::cout << "Length string: " << myString.length() << std::endl;
+    std::cout << "First symbol: " << myString.front() << std::endl;
+    std::cout << "Last symbol: " << myString.back() << std::endl;
+    return 0;
 }
